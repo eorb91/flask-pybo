302989230a54f8f4e17b17aee652cf143140d209
강대규 <eorb91@nate.com>
2020-12-29 10:36:13 +0900

    404 오류등록

--- config/production.py
+++ config/production.py
@@ -1,8 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = "dev"
+SECRET_KEY = b'\xaa\xb75\x8fi\x16\x1fEq\xd1\x01?\x9e\xc8\x836'
